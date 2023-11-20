4aed2947f48d5b88c2ee632d73d79ce896aef4f0
Gustavout22 <gustavougartetorres@gmail.com>
2023-11-20 18:27:52 -0500

    using 2 states in the same function component

--- src/hooks/useState.jsx
+++ src/hooks/useState.jsx
@@ -1,20 +1,26 @@
 import { useState } from "react";
 
 function UsingUseState() {
-  const [state, setState] = useState({ count: 4, theme: "blue" });
-  const { count } = state;
-  const { theme } = state;
+  // const [state, setState] = useState({ count: 4, theme: "blue" });
+  // const { count } = state;
+  // const { theme } = state;
+  const [count, setCount] = useState(5);
+  const [theme, setTheme] = useState("blue");
 
   function decrementCount() {
-    setState((prevState) => {
-      return { ...prevState, count: prevState.count - 1 };
-    });
+    // setState((prevState) => {
+    //   return { ...prevState, count: prevState.count - 1 };
+    // });
+    setCount((prevState) => prevState - 1);
+    setTheme("red");
   }
 
   function incrementCount() {
-    setState((prevState) => {
-      return { ...prevState, count: prevState.count + 1 };
-    });
+    // setState((prevState) => {
+    //   return { ...prevState, count: prevState.count + 1 };
+    // });
+    setCount((prevState) => prevState + 1);
+    setTheme("green");
   }
 
   return (
